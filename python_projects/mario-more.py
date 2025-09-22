42715365a82d7f9ab8dccb76aee728fe991737aa
morady <morady515@gmail.com>
2025-09-22 23:36:03 +0300

    add more python_problems

---
+++ python_projects/mario-more.py
@@ -0,0 +1,14 @@
+import basicfunc
+def main():
+    while True:
+        height = basicfunc.get_int("Height: ")
+        if height >= 1 and height <= 8:
+            break
+    space = height - 1
+    for i in range(height):
+        print(" " * space,end = "")
+        space -= 1
+        print("#" * (i + 1),end = "")
+        print(" ","#" * (i + 1))
+
+main()
